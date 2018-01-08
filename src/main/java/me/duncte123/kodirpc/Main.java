ff09bdecca982645443dcf0461064b673a04edcc
Duncan Sterken <duncan.sterken123@gmail.com>
2018-01-08 18:16:36 +0100

    Add all files

---
+++ src/main/java/me/duncte123/kodirpc/Main.java
@@ -0,0 +1,58 @@
+package me.duncte123.kodirpc;
+
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private static DiscordRPC lib = DiscordRPC.INSTANCE;
+
+    public static void main(String... args) {
+        String applicationId = "396990609358979074";
+        String steamId = "";
+        DiscordEventHandlers handlers = new DiscordEventHandlers();
+        handlers.ready = () -> System.out.println("Ready!");
+        lib.Discord_Initialize(applicationId, handlers, true, steamId);
+        DiscordRichPresence presence = new DiscordRichPresence();
+        //presence.startTimestamp = System.currentTimeMillis() / 1000; // epoch second
+        presence.details = "Idling";
+        presence.largeImageKey = "logo";
+        lib.Discord_UpdatePresence(presence);
+        // in a worker thread
+        new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                lib.Discord_RunCallbacks();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ignored) {}
+            }
+        }, "RPC-Callback-Handler").start();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .readTimeout(0,  TimeUnit.MILLISECONDS)
+                .build();
+        WebListener wl = new WebListener();
+        Request request = new Request.Builder()
+                .url("ws://localhost:9090/jsonrpc")
+                .build();
+        WebSocket ws = client.newWebSocket(request, wl);
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                ws.close(1000, "Exit");
+                client.dispatcher().executorService().shutdown();
+            }
+        });
+
+    }
+
+    public static DiscordRPC getLib() {
+        return lib;
+    }
+}
---
+++ src/main/java/me/duncte123/kodirpc/WebListener.java
@@ -0,0 +1,51 @@
+package me.duncte123.kodirpc;
+
+import club.minnced.discord.rpc.DiscordRichPresence;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+import org.json.JSONObject;
+
+
+public class WebListener extends WebSocketListener {
+
+    @Override public void onOpen(WebSocket webSocket, Response response) {
+        System.out.println("Connected");
+        //webSocket.close(1000, "Goodbye, World!");
+    }
+
+    @Override public void onMessage(WebSocket webSocket, String text) {
+        //System.out.println("MESSAGE: " + text);
+        JSONObject jsonObject = new JSONObject(text);
+        System.out.println(jsonObject.toString(4));
+        DiscordRichPresence presence = new DiscordRichPresence();
+        presence.details = "Idling";
+        presence.largeImageKey = "logo";
+        if(jsonObject.optString("method") != null && jsonObject.optString("method").equals("System.OnQuit")) {
+            System.exit(0);
+            return;
+        }
+        if(jsonObject.optString("method") != null && jsonObject.optString("method").equals("Player.OnPlay")) {
+            JSONObject item = jsonObject.optJSONObject("params").optJSONObject("data").optJSONObject("item");
+            presence.details = "Watching " +  item.optString("showtitle");
+            presence.state = item.getString("title");
+        }
+        Main.getLib().Discord_UpdatePresence(presence);
+    }
+
+    @Override public void onMessage(WebSocket webSocket, ByteString bytes) {
+        System.out.println("MESSAGE: " + bytes.hex());
+    }
+
+    @Override public void onClosing(WebSocket webSocket, int code, String reason) {
+        webSocket.close(1000, null);
+        System.out.println("CLOSE: " + code + " " + reason);
+    }
+
+    @Override public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+        t.printStackTrace();
+    }
+
+
+}
